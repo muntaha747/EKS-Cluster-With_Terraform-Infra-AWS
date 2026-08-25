86fbf1ed14e8648f802495bc67224b8b0a3b4275
muntaha747 <muntaha747@gmail.com>
2026-08-25 11:22:51 -0400

    finalized-version

--- nat-gateway.tf
+++ nat-gateway.tf
@@ -7,7 +7,7 @@ resource "aws_eip" "static_ip" {
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.static_ip.id
-  subnet_id     = aws_subnet.Public_subnet_Zone1.id
+  subnet_id     = aws_subnet.Public_subnet-1_Zone1.id
   tags = {
     Name = "${local.env}-Nat-Gateway"
   }
--- route-tables.tf
+++ route-tables.tf
@@ -14,24 +14,30 @@ resource "aws_route_table" "private_route_table" {
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.EKS-VPC.id
   route {
-    cidr_block     = "0.0.0.0/0"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
   tags = {
     Name = "${local.env}-public-route-table"
   }
 }
-
-
 resource "aws_route_table_association" "private_subnet-1_association" {
   subnet_id      = aws_subnet.Private_subnet-1_Zone1.id
   route_table_id = aws_route_table.private_route_table.id
 }
 
 resource "aws_route_table_association" "private_subnet-2_association" {
-  subnet_id      = aws_subnet.Private_subnet-2_Zone2
+  subnet_id      = aws_subnet.Private_subnet-2_Zone2.id
   route_table_id = aws_route_table.private_route_table.id
 }
 
 
+resource "aws_route_table_association" "public_subnet-1_association" {
+  subnet_id      = aws_subnet.Public_subnet-1_Zone1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
 
+resource "aws_route_table_association" "public_subnet-2_association" {
+  subnet_id      = aws_subnet.Public_subnet-2_Zone2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
